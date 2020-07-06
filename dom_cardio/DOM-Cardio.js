5a6922402438074065dc41b234a7b627440a1c52
Natacha <natacha.har@onja.org>
2020-07-06 13:42:21 +0300

    create Html using DOM

--- dom_cardio/DOM-Cardio.js
+++ dom_cardio/DOM-Cardio.js
@@ -1,26 +1,48 @@
 // Make a div
+const newDiv = document.createElement('div');
 
 // add a class of wrapper to it
+newDiv.classList.add('cards');
 
 // put it into the body
+document.body.appendChild('newDiv');
 
 // make an unordered list
 
+const unorderedList = document.createElement('ul');
+const firstList = document.createElement('li');
+const secondList = document.createElement('li');
+const thirdList = document.createElement('li');
+
+
 // add three list items with the words "one, two three" in them
+firstList.textContent = 'one';
+secondList.textContent = 'two';
+thirdList.textContent = 'three';
 
 // put that list into the above wrapper
+unorderedList.appendChild(firstList);
+unorderedList.appendChild(secondList);
+unorderedList.appendChild(thirdList);
+
 
 // create an image
+const image = document.createElement('img');
 
 // set the source to an image
+image.setAttribute('src', 'https://picsum.photos/');
 
 // set the width to 250
+const width = 250;
 
 // add a class of cute
+image.classList.add('cute');
 
 // add an alt of Cute Puppy
+image.setAttribute('alt', 'Cute Puppy');
 
 // Append that image to the wrapper
+
 
 // with HTML string, make a div, with two paragraphs inside of it
 // put this div before the unordered list from above
